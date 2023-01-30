3ec9cbc67b27e4062e8b8cd4a076cb9cf3b1c831
Angela <angeladev@outlook.com.br>
2023-01-29 21:49:23 -0300

    update

--- js/minhalista.js
+++ js/minhalista.js
@@ -1,6 +1,93 @@
-let add1 = getElementById ("add1");
-let card1 = getElementById ("card1");
+const content = document.querySelector(".content");
+const inputSearch = document.querySelector("input[type='search']");
 
-function minhaLista() {
-    document.innerHTML = "card1";
+let items = [];
+
+inputSearch.oninput = () => {
+  content.innerHTML = "";
+
+  items
+    .filter((item) =>
+      item.toLowerCase().includes(inputSearch.value.toLowerCase())
+    )
+    .forEach((item) => addHTML(item));
+};
+
+function addHTML(item) {
+  const div = document.createElement("div");
+  div.innerHTML = item;
+  content.append(div);
 }
+
+fetch("https://cinema-servidor-03.onrender.com/terror")
+.then((data) => data.json())
+.then((terror) => {
+  terror.forEach((user) => {
+    addHTML(user.nome);
+    items.push(user.nome);
+  });
+});
+
+fetch("https://cinema-servidor-03.onrender.com/acao")
+
+.then((data) => data.json())
+.then((acao) => {
+  acao.forEach((user) => {
+    addHTML(user.nome);
+    items.push(user.nome);
+  });
+});
+
+fetch("https://cinema-servidor-03.onrender.com/drama")
+.then((data) => data.json())
+.then((drama) => {
+  drama.forEach((user) => {
+    addHTML(user.nome);
+    items.push(user.nome);
+  });
+});
+
+fetch("https://cinema-servidor-03.onrender.com/comedia")
+.then((data) => data.json())
+.then((comedia) => {
+  comedia.forEach((user) => {
+    addHTML(user.nome);
+    items.push(user.nome);
+  });
+});
+
+fetch("https://cinema-servidor-03.onrender.com/romance")
+.then((data) => data.json())
+.then((romance) => {
+  romance.forEach((user) => {
+    addHTML(user.nome);
+    items.push(user.nome);
+  });
+});
+
+fetch("https://projetoindividual-2otg.onrender.com/Romance")
+.then((data) => data.json())
+.then((Romance) => {
+  Romance.forEach((user) => {
+    addHTML(user.titulo);
+    items.push(user.titulo);
+  });
+});
+
+fetch("https://projetoindividual-2otg.onrender.com/investigacao-criminal")
+.then((data) => data.json())
+.then((investigacaocriminal) => {
+    investigacaocriminal.forEach((user) => {
+    addHTML(user.titulo);
+    items.push(user.titulo);
+  });
+});
+
+fetch("https://projetoindividual-2otg.onrender.com/Auto-ajuda")
+.then((data) => data.json())
+.then((Autoajuda) => {
+    Autoajuda.forEach((user) => {
+    addHTML(user.titulo);
+    items.push(user.titulo);
+  });
+});
